ee6dbd7ae56b0a3d14f365f62290cf7754ad19e5
havakaracam <havakaracam@marun.edu.tr>
2018-10-24 13:20:40 +0300

    classlar ve variablelar eklendi

---
+++ src/Player.java
@@ -0,0 +1,13 @@
+public class Player {
+    private int id;
+    private int money;
+    private int totalDebt;
+    private Property property[]=new Property [60];
+    private int totalAssetPrice;
+    private int debtToPlayers;
+    private int debtToBanks;
+    int currentLocation;
+    private boolean isJail=false;
+    private boolean isBankruptcy=false;
+
+}
---
+++ src/Property.java
@@ -0,0 +1,11 @@
+class Property {
+    private int price;
+    private int rent;
+    private int housePrice;
+    private int house;
+    private int mortgagePrice;
+    String color;
+    boolean isMortgage=false;
+    boolean hasOwner=false;
+
+}
